ccbed96badf1c844521f3d540febaca914f1bdda
S <145682660+sshania@users.noreply.github.com>
2024-06-12 01:18:06 +0700

    login

--- Project_Backend_Ureeka/routes/web.php
+++ Project_Backend_Ureeka/routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/book', [BookController::class, 'index'])->name('book.index');
-Route::get('/book/create', [BookController::class, 'create'])->name('book.create');
-Route::post('/book', [BookController::class, 'store'])->name('book.store');
-Route::get('/book/{book}/edit', [BookController::class, 'edit'])->name('book.edit');
-Route::put('/book/{book}/update', [BookController::class, 'update'])->name('book.update');
-Route::delete('/book/{book}/destroy', [BookController::class, 'destroy'])->name('book.destroy');
+Route::get('/register', [LoginController::class, 'register'])->name('register');
+Route::post('/registerUser', [LoginController::class, 'registerUser'])->name('registerUser');
+
+Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/loginProcess', [LoginController::class, 'loginProcess'])->name('loginProcess');
+
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('/cobaLogout', [LoginController::class, 'cobaLogout'])->name('cobaLogout');
